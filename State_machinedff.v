3c30ad5f9075718219fcc4c61b16b226918bb36b
Caleb Cook <gtcookv@gmail.com>
2025-04-15 11:30:19 -0600

    Update State_machinedff.v

--- State_machinedff.v
+++ State_machinedff.v
@@ -1,1 +1,20 @@
+module State_machinedff(
+    input Default,
+    input D,
+    input clk,
+    input Reset,
+    output reg Q
+);
 
+    initial begin
+        Q <= Default;
+    end
+
+    always @(posedge clk, posedge Reset) begin
+        if (Reset)
+            Q <= Default;
+        else
+            Q <= D;
+    end
+
+endmodule
