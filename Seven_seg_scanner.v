5f35712f763c1307ac3e37771407856dde3d3d31
Caleb Cook <178955176+Caleb-ops-sudo@users.noreply.github.com>
2025-04-26 20:10:20 -0600

    Update Seven_seg_scanner.v

--- Seven_seg_scanner.v
+++ Seven_seg_scanner.v
@@ -1,45 +1,49 @@
 module seven_seg_scanner(
     input div_clock,
-    output [3:0] anode,
-    input Reset
-    
+    input Reset,
+    output [3:0] anode
+
 );
- wire Slow [3:0];
- 
+//becuase the anodes on the 7 seg displays are active when low 
+assign anode = ~state;
 
-  State_machinedff Adff(
-        .Default(1'b0),
-        .D(Slow[0]),
-        .clk(div_clock),
-        .Q(anode[0]),
-        .Reset(Reset)
-    );
+wire[3:0] next;
+wire[3:0] state;
 
-    State_machinedff Bdff(
-        .Default(1'b1),
-        .D(Slow[1]),
-        .clk(div_clock),
-        .Q(anode[1]),
-        .Reset(Reset)
-    );
+State_machinedff Adff(
+    .D(next[0]),
+    .Q(state[0]),
+    .clk(div_clock),
+    .Reset(Reset),
+    .Default(1'b1)
+);
+State_machinedff Bdff(
+    .D(next[1]),
+    .Q(state[1]),
+    .clk(div_clock),
+    .Reset(Reset),
+    .Default(1'b0)
+);
+State_machinedff Cdff(
+    .D(next[2]),
+    .Q(state[2]),
+    .clk(div_clock),
+    .Reset(Reset),
+    .Default(1'b0)
+);
+State_machinedff Ddff(
+    .D(next[3]),
+    .Q(state[3]),
+    .clk(div_clock),
+    .Reset(Reset),
+    .Default(1'b0)
+);
 
-    State_machinedff Cdff(
-        .Default(1'b1),
-        .D(Slow[2]),
-        .clk(div_clock),
-        .Q(anode[2]),
-        .Reset(Reset)
-    );
-    
-    State_machinedff Ddff(
-       .Default(1'b1),
-       .D(Slow[3]),
-       .clk(div_clock),
-       .Q(anode[3]),
-       .Reset(Reset)
-        
-    );
-    
+assign next[0] = state[3];
+assign next[1] = state[0];
+assign next[2] = state[1];
+assign next[3] = state[2];
+
 
 
 endmodule
